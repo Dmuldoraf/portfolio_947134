f8611deb4501cdcc2e81421746a4bf6f1db7a196
Philipp Lind <plind@tutanota.com>
2024-09-12 13:41:37 +0200

    refactor: change bg to mint

--- lib/presentation/home_page/widgets/home_container.dart
+++ lib/presentation/home_page/widgets/home_container.dart
@@ -17,7 +17,9 @@ class HomeContainer extends StatelessWidget {
               minHeight: 1000,
               minWidth: MediaQuery.of(context).size.width,
             ),
-            child: Image.asset('assets/flowers.png',
+            //mint_5_noise.png
+            // flowers.png
+            child: Image.asset('assets/mint_5_noise.png',
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
                 fit: BoxFit.cover),
